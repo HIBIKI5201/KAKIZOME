acd881f591ad4afc92c95f726ccdc1b851abc8ec
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-05 21:43:58 +0900

    [add]フェーズ２のシステムを抽出

---
+++ Assets/Scripts/EntityComponent/System/Phase2UpdateSystem.cs
@@ -0,0 +1,61 @@
+using Unity.Burst;
+using Unity.Entities;
+
+namespace Master.Entities
+{
+    /// <summary>
+    /// フェーズ2から3への移行を処理するシステム。
+    /// </summary>
+    [DisableAutoCreation]
+    [UpdateInGroup(typeof(ParticleSystemGroup))]
+    [UpdateAfter(typeof(Phase1UpdateSystem))]
+    public partial struct Phase2UpdateSystem : ISystem
+    {
+        private EntityQuery _phase2EntityQuery;
+
+        public void OnCreate(ref SystemState state)
+        {
+            _phase2EntityQuery = state.GetEntityQuery(
+                ComponentType.ReadWrite<ParticleEntity>(),
+                ComponentType.ReadWrite<Phase2TimerEntity>()
+            );
+            state.RequireForUpdate<GlobalState>();
+            state.RequireForUpdate(_phase2EntityQuery);
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+
+            var job = new Phase2UpdateJob
+            {
+                ECB = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter(),
+                DeltaTime = SystemAPI.Time.DeltaTime
+            };
+
+            state.Dependency = job.ScheduleParallel(_phase2EntityQuery, state.Dependency);
+        }
+    }
+
+    [BurstCompile]
+    public partial struct Phase2UpdateJob : IJobEntity
+    {
+        public float DeltaTime;
+        public EntityCommandBuffer.ParallelWriter ECB;
+
+        void Execute(
+            [EntityIndexInQuery] int entityIndexInQuery,
+            Entity entity,
+            ref ParticleEntity particle,
+            ref Phase2TimerEntity timer)
+        {
+            timer.ElapsedTime += DeltaTime;
+
+            if (timer.Timer < timer.ElapsedTime)
+            {
+                particle.Phase = 3;
+                ECB.RemoveComponent<Phase2TimerEntity>(entityIndexInQuery, entity);
+            }
+        }
+    }
+}
