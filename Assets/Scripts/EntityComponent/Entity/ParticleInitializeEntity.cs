bd440650776662c79652b015a327aa130ef7e131
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-05 17:34:39 +0900

    [update]初期化用のエンティティを追加

---
+++ Assets/Scripts/EntityComponent/Entity/ParticleInitializeEntity.cs
@@ -0,0 +1,20 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace Master.Entities
+{
+    /// <summary>
+    ///     パーティクル初期化エンティティコンポーネント。
+    /// </summary>
+    public readonly struct ParticleInitializeEntity : IComponentData
+    {
+        public ParticleInitializeEntity(int count, float phase1Duration)
+        {
+            Count = count;
+            Phase1Duration = phase1Duration;
+        }
+
+        public readonly int Count;
+        public readonly float Phase1Duration;
+    }
+}
