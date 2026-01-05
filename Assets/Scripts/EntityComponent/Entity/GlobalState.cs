a27a51f01616fd08bff1dc94f594dfe4306d230d
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-05 15:20:59 +0900

    [update]フェーズの配列を持つように

--- Assets/Scripts/EntityComponent/Entity/GlobalState.cs
+++ Assets/Scripts/EntityComponent/Entity/GlobalState.cs
@@ -1,16 +1,41 @@
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 
 namespace Master.Entities
 {
-    public struct GlobalState : IComponentData
+    public struct GlobalState : IComponentData, INativeDisposable
     {
         public GlobalState(int count, float phase1Duration)
         {
             Count = count;
             Phase1Duration = phase1Duration;
+
+            PhaseArray = new NativeArray<int>(count, Allocator.Persistent);
         }
 
         public readonly int Count;
         public readonly float Phase1Duration;
+
+        public readonly NativeArray<int> PhaseArray;
+
+        public JobHandle Dispose(JobHandle inputDeps)
+        {
+            if (PhaseArray.IsCreated)
+            {
+                return PhaseArray.Dispose(inputDeps);
+            }
+
+            return inputDeps;
+        }
+
+        // 即時解放
+        public void Dispose()
+        {
+            if (PhaseArray.IsCreated)
+            {
+                PhaseArray.Dispose();
+            }
+        }
     }
 }
