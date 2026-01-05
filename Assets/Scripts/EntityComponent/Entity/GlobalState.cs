883cf2c3a77a73326bac6ce2fc800e88be7c97bb
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-05 20:55:36 +0900

    [update]フェーズ２データをステートに追加

--- Assets/Scripts/EntityComponent/Entity/GlobalState.cs
+++ Assets/Scripts/EntityComponent/Entity/GlobalState.cs
@@ -8,11 +8,12 @@ namespace Master.Entities
     public struct GlobalState : IComponentData, INativeDisposable
     {
         public GlobalState(int count, int kernelValue,
-            Phase1Configs phase1Configs)
+            Phase1Configs phase1Configs, Phase2Configs phase2Configs)
         {
             Count = count;
             KernelValue = kernelValue;
             Phase1Configs = phase1Configs;
+            Phase2Configs = phase2Configs;
 
             PhaseCountArray = new NativeArray<int>(count, Allocator.Persistent);
         }
@@ -20,6 +21,7 @@ namespace Master.Entities
         public readonly int Count;
         public readonly int KernelValue;
         public readonly Phase1Configs Phase1Configs;
+        public readonly Phase2Configs Phase2Configs;
 
         public NativeArray<int> PhaseCountArray;
 
