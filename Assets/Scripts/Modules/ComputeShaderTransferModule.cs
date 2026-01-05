f86b5a71050962034b3b743bc7e3aad8cd8b07c3
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-05 22:46:12 +0900

    [update]色バッファをシェーダーにバインドするように

--- Assets/Scripts/Modules/ComputeShaderTransferModule.cs
+++ Assets/Scripts/Modules/ComputeShaderTransferModule.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Entities.UniversalDelegates;
 using UnityEngine;
 
 namespace Master.Modules
@@ -15,12 +16,13 @@ namespace Master.Modules
             _data = data;
 
             // カーネルインデックス取得。
-            PhaseKernelData[] kernelDatas = data.KernelDastas;
-            _kernelIndexs = new int[kernelDatas.Length];
+            PhaseKernelData[] kernelDatas = data.PhaseKernelDastas;
+            _phaseKernelIndexs = new int[kernelDatas.Length];
             for (int i = 0; i < kernelDatas.Length; i++)
             {
-                _kernelIndexs[i] = data.Shader.FindKernel(kernelDatas[i].KernelName);
+                _phaseKernelIndexs[i] = data.Shader.FindKernel(kernelDatas[i].KernelName);
             }
+            _colorKernelIndex = data.Shader.FindKernel(data.ColorKernelName);
         }
 
         public void BindParameter(
@@ -30,14 +32,15 @@ namespace Master.Modules
             float speed,
             float stopDistance)
         {
-            for (int i = 0; i < _kernelIndexs.Length; i++)
+            for (int i = 0; i < _phaseKernelIndexs.Length; i++)
             {
-                int index = _kernelIndexs[i];
+                int index = _phaseKernelIndexs[i];
                 _data.Shader.SetBuffer(index, _data.PositionBufferName, bufferContainer.PositionBuffer);
                 _data.Shader.SetBuffer(index, _data.VelocityBufferName, bufferContainer.VelocityBuffer);
                 _data.Shader.SetBuffer(index, _data.TargetBufferName, bufferContainer.TargetBuffer);
+                _data.Shader.SetBuffer(index, _data.ColorBufferName, bufferContainer.ColorBuffer);
 
-                _data.Shader.SetBuffer(index, _data.KernelDastas[i].IndexBufferName, bufferContainer.PhaseIndicesBuffers[i]);
+                _data.Shader.SetBuffer(index, _data.PhaseKernelDastas[i].IndexBufferName, bufferContainer.PhaseIndicesBuffers[i]);
             }
 
             _data.Shader.SetInt(_data.ParticleCountName, count);
@@ -47,23 +50,29 @@ namespace Master.Modules
             _data.Shader.SetFloat(_data.StopDistanceName, stopDistance);
         }
 
-        public void Dispatch(float deltaTime, float time, GraphicsBuffer[] phaseBuffers, ReadOnlySpan<int> counts)
+        public void Dispatch(float deltaTime, float time,
+            GraphicsBuffer[] phaseBuffers, ReadOnlySpan<int> counts,
+            int count)
         {
             _data.Shader.SetFloat(_data.DeltaTimeName, deltaTime);
             _data.Shader.SetFloat(_data.TimeName, time);
 
-            for (int i = 0; i < _kernelIndexs.Length; i++)
+            for (int i = 0; i < _phaseKernelIndexs.Length; i++)
             {
                 if (counts[i] == 0) { continue; }
 
                 int threadGroups = Mathf.CeilToInt(counts[i] / 64f);
 
-                _data.Shader.SetInt(_data.KernelDastas[i].CounterName, counts[i]);
-                _data.Shader.Dispatch(_kernelIndexs[i], threadGroups, 1, 1);
+                _data.Shader.SetInt(_data.PhaseKernelDastas[i].CounterName, counts[i]);
+                _data.Shader.Dispatch(_phaseKernelIndexs[i], threadGroups, 1, 1);
             }
+
+            int tg = Mathf.CeilToInt(count / 256f);
+            _data.Shader.Dispatch(_colorKernelIndex, tg, 1, 1);
         }
 
-        private readonly int[] _kernelIndexs;
+        private readonly int[] _phaseKernelIndexs;
+        private readonly int _colorKernelIndex;
         private readonly ComputeShaderData _data;
 
         [Serializable]
@@ -85,11 +94,13 @@ namespace Master.Modules
         public struct ComputeShaderData
         {
             public ComputeShader Shader => _shader;
-            public PhaseKernelData[] KernelDastas => _kernelNames;
+            public PhaseKernelData[] PhaseKernelDastas => _phaseKernelNames;
+            public string ColorKernelName => _colorKernelName;
             public string PositionBufferName => _positionBufferName;
             public string VelocityBufferName => _velocityBufferName;
             public string TargetBufferName => _targetBufferName;
             public string PhaseBufferName => _phaseBufferName;
+            public string ColorBufferName => _colorBufferName;
             public string ParticleCountName => _particleCountName;
             public string CenterPositionName => _centerPositionName;
             public string RotationRadiusName => _rotationRadiusName;
@@ -101,11 +112,13 @@ namespace Master.Modules
             public static void Assert(ComputeShaderData data)
             {
                 Debug.Assert(data.Shader != null, "ComputeShader is null");
-                Debug.Assert(data.KernelDastas.Length != 0, $"{nameof(KernelDastas)} length is 0");
+                Debug.Assert(data.PhaseKernelDastas.Length != 0, $"{nameof(PhaseKernelDastas)} length is 0");
+                Debug.Assert(!string.IsNullOrEmpty(data.ColorKernelName), $"{nameof(PositionBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.PositionBufferName), $"{nameof(PositionBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.VelocityBufferName), $"{nameof(VelocityBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.TargetBufferName), $"{nameof(TargetBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.PhaseBufferName), $"{nameof(PhaseBufferName)} is null");
+                Debug.Assert(!string.IsNullOrEmpty(data.ColorBufferName), $"{nameof(PhaseBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.ParticleCountName), $"{nameof(ParticleCountName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.CenterPositionName), $"{nameof(CenterPositionName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.RotationRadiusName), $"{nameof(RotationRadiusName)} is null");
@@ -119,8 +132,10 @@ namespace Master.Modules
             private ComputeShader _shader;
 
             [Space]
-            [SerializeField, Tooltip("カーネル名")]
-            private PhaseKernelData[] _kernelNames;
+            [SerializeField, Tooltip("フェーズのカーネル名")]
+            private PhaseKernelData[] _phaseKernelNames;
+            [SerializeField, Tooltip("")]
+            private string _colorKernelName;
 
             [Space]
             [SerializeField, Tooltip("位置バッファ名")]
@@ -131,6 +146,8 @@ namespace Master.Modules
             private string _targetBufferName;
             [SerializeField, Tooltip("フェーズバッファ名")]
             private string _phaseBufferName;
+            [SerializeField, Tooltip("色バッファ名")]
+            private string _colorBufferName;
             [SerializeField, Tooltip("パーティクル数パラメータ名")]
             private string _particleCountName;
             [SerializeField, Tooltip("中心位置パラメータ名")]
