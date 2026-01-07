03012060335f4689a76fd958e1689d12647d7433
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-07 21:00:13 +0900

    [update]円環半径を追加

--- Assets/Scripts/Configs/Phase1Configs.cs
+++ Assets/Scripts/Configs/Phase1Configs.cs
@@ -9,14 +9,10 @@ namespace Master.Configs
     {
         public float Duration => _duration;
         public Vector2 DurationRange => _durationRange;
-        public float RotationRadius => _rotationRadius;
 
         [SerializeField, Tooltip("フェーズ１の長さ")]
         private float _duration;
         [SerializeField, Tooltip("フェーズ１の長さの幅"), MinMaxRangeSlider(-10, 10)]
         private Vector2 _durationRange;
-
-        [SerializeField, Tooltip("回転する半径")]
-        private float _rotationRadius;
     }
 }
--- Assets/Scripts/Configs/Phase2Configs.cs
+++ Assets/Scripts/Configs/Phase2Configs.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace Master.Configs
@@ -7,8 +8,12 @@ namespace Master.Configs
     public struct Phase2Configs
     {
         public float Duration => _duration;
+        public float TorusMaxRadius => _torusRadius.y;
+        public float TorusMinRadius => _torusRadius.x;
 
         [SerializeField, Tooltip("フェーズ２の時間")]
         private float _duration;
+        [SerializeField, Tooltip("円環の半径"), MinMaxRangeSlider(0, 30)]
+        private Vector2 _torusRadius;
     }
 }
