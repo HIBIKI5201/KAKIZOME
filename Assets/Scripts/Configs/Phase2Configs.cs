8032ae4a0287127e62761ef57fab4ada05f37c1f
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-07 23:57:07 +0900

    [update]フェーズ2のギズモを追加

--- Assets/Scripts/Configs/Phase2Configs.cs
+++ Assets/Scripts/Configs/Phase2Configs.cs
@@ -7,12 +7,42 @@ namespace Master.Configs
     [Serializable]
     public struct Phase2Configs
     {
-        public float Duration => _duration;
+        public float DurationToPhase3 => _durationToPhase3;
+        public float DurationToPhaseFinal => _durationToPhaseFinal;
+        public float TorusSpeed => _torusSpeed;
         public float TorusMaxRadius => _torusRadius.y;
         public float TorusMinRadius => _torusRadius.x;
 
-        [SerializeField, Tooltip("フェーズ２の時間")]
-        private float _duration;
+        public static void OnDrawGizmos(Phase2Configs configs, Vector3 center)
+        {
+            const int VERTEX_COUNT = 360;
+
+            Span<Vector3> inPos = stackalloc Vector3[VERTEX_COUNT];
+            Span<Vector3> outPos = stackalloc Vector3[VERTEX_COUNT];
+
+            for (int i = 0; i < VERTEX_COUNT; i++)
+            {
+                float t = (float)i / VERTEX_COUNT;
+                float rad = t * Mathf.PI * 2f;
+
+                float cos = Mathf.Cos(rad);
+                float sin = Mathf.Sin(rad);
+
+                inPos[i] = new Vector3(sin * configs.TorusMinRadius, 0, cos * configs.TorusMinRadius) + center;
+                outPos[i] = new Vector3(sin * configs.TorusMaxRadius, 0, cos * configs.TorusMaxRadius) + center;
+            }
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLineList(inPos);
+            Gizmos.DrawLineList(outPos);
+        }
+
+        [SerializeField, Tooltip("フェーズ３までの時間")]
+        private float _durationToPhase3;
+        [SerializeField, Tooltip("フェーズファイナルまでの時間")]
+        private float _durationToPhaseFinal;
+        [SerializeField, Tooltip("円環の回転速度")]
+        private float _torusSpeed;
         [SerializeField, Tooltip("円環の半径"), MinMaxRangeSlider(0, 30)]
         private Vector2 _torusRadius;
     }
