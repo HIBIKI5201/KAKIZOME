20fd21431be772a7c4676fd6695d3a07f6455fc4
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-08 21:14:47 +0900

    [update]シェーダーのパラメータを追加

--- Assets/Scripts/Modules/ComputeShaderTransferModule.cs
+++ Assets/Scripts/Modules/ComputeShaderTransferModule.cs
@@ -56,13 +56,17 @@ namespace Master.Modules
             // グローバル。
             shader.SetInt(_data.ParticleCountName, count);
             shader.SetVector(_data.CenterPositionName, phaseConfig.GlobalConfigs.CenterPosition);
-            shader.SetFloat(_data.SpeedName, phaseConfig.GlobalConfigs.ParticleSpeed);
-            
+
+            // フェーズ1。
+            shader.SetFloat(_data.Phase1AccelName, phaseConfig.Phase1Configs.Accel);
+
             // フェーズ2。
             shader.SetFloat(_data.TorusMaxRadiusName, phaseConfig.Phase2Configs.TorusMaxRadius);
             shader.SetFloat(_data.TorusMinRadiusName, phaseConfig.Phase2Configs.TorusMinRadius);
             shader.SetFloat(_data.TorusSpeedName, phaseConfig.Phase2Configs.TorusSpeed);
             shader.SetFloat(_data.TorusYSpringName, phaseConfig.Phase2Configs.TorusYSpring);
+            shader.SetFloat(_data.TorusTangentGainName, phaseConfig.Phase2Configs.TangentGain);
+            shader.SetFloat(_data.TorusRadiusGainName, phaseConfig.Phase2Configs.RadiusGain);
 
             // フェーズ3。
             shader.SetFloat(_data.OrbitalSphereCountName, phaseConfig.Phase3Configs.OrbitalSphereCount);
@@ -71,9 +75,12 @@ namespace Master.Modules
             shader.SetFloat(_data.OrbitalSpeedName, phaseConfig.Phase3Configs.OrbitalSpeed);
             shader.SetFloat(_data.OrbitalMaxRadiusName, phaseConfig.Phase3Configs.OrbitalMaxRadius);
             shader.SetFloat(_data.OrbitalMinRadiusName, phaseConfig.Phase3Configs.OrbitalMinRadius);
+            shader.SetFloat(_data.OrbitalPosGainName, phaseConfig.Phase3Configs.PosGain);
+            shader.SetFloat(_data.OrbitalVelGainName, phaseConfig.Phase3Configs.VelGain);
 
             // フェーズファイナル。
             shader.SetFloat(_data.StopDistanceName, phaseConfig.PhaseFinalConfigs.StopDistance);
+            shader.SetFloat(_data.PhaseFinalFollowGainName, phaseConfig.PhaseFinalConfigs.FollowGain);
             #endregion
         }
 
@@ -128,22 +135,37 @@ namespace Master.Modules
             public string TargetBufferName => _targetBufferName;
             public string PhaseBufferName => _phaseBufferName;
             public string ColorBufferName => _colorBufferName;
+            
+            // グローバル。
             public string ParticleCountName => _particleCountName;
-            public string SpeedName => _speedName;
             public string CenterPositionName => _centerPositionName;
             public string TimeName => _timeName;
             public string DeltaTimeName => _deltaTimeName;
+
+            // フェーズ1。
+            public string Phase1AccelName => _phase1AccelName;
+
+            // フェーズ2。
             public string TorusMaxRadiusName => _torusMaxRadiusName;
             public string TorusMinRadiusName => _torusMinRadiusName;
             public string TorusSpeedName => _torusSpeedName;
             public string TorusYSpringName => _torusYSpringName;
+            public string TorusTangentGainName => _torusTangentGainName;
+            public string TorusRadiusGainName => _torusRadiusGainName;
+
+            // フェーズ3。
             public string OrbitalSphereCountName => _orbitalSphereCountName;
             public string OrbitalSphereRadiusName => _orbitalSphereRadiusName;
             public string OrbitalCenterOffsetName => _orbitalCenterOffsetName;
             public string OrbitalSpeedName => _orbitalSpeedName;
             public string OrbitalMaxRadiusName => _orbitalMaxRadiusName;
             public string OrbitalMinRadiusName => _orbitalMinRadiusName;
+            public string OrbitalPosGainName => _orbitalPosGainName;
+            public string OrbitalVelGainName => _orbitalVelGainName;
+
+            // フェーズファイナル。
             public string StopDistanceName => _stopDistanceName;
+            public string PhaseFinalFollowGainName => _phaseFinalFollowGainName;
 
             public static void Assert(ComputeShaderData data)
             {
@@ -156,21 +178,30 @@ namespace Master.Modules
                 Debug.Assert(!string.IsNullOrEmpty(data.PhaseBufferName), $"{nameof(PhaseBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.ColorBufferName), $"{nameof(ColorBufferName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.ParticleCountName), $"{nameof(ParticleCountName)} is null");
-                Debug.Assert(!string.IsNullOrEmpty(data.SpeedName), $"{nameof(SpeedName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.CenterPositionName), $"{nameof(CenterPositionName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.TimeName), $"{nameof(TimeName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.DeltaTimeName), $"{nameof(DeltaTimeName)} is null");
+
+                Debug.Assert(!string.IsNullOrEmpty(data.Phase1AccelName), $"{nameof(Phase1AccelName)} is null");
+
                 Debug.Assert(!string.IsNullOrEmpty(data.TorusMaxRadiusName), $"{nameof(TorusMaxRadiusName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.TorusMinRadiusName), $"{nameof(TorusMinRadiusName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.TorusSpeedName), $"{nameof(TorusSpeedName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.TorusYSpringName), $"{nameof(TorusYSpringName)} is null");
+                Debug.Assert(!string.IsNullOrEmpty(data.TorusTangentGainName), $"{nameof(TorusTangentGainName)} is null");
+                Debug.Assert(!string.IsNullOrEmpty(data.TorusRadiusGainName), $"{nameof(TorusRadiusGainName)} is null");
+                
                 Debug.Assert(!string.IsNullOrEmpty(data.OrbitalSphereCountName), $"{nameof(OrbitalSphereCountName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.OrbitalSphereRadiusName), $"{nameof(OrbitalSphereRadiusName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.OrbitalCenterOffsetName), $"{nameof(OrbitalCenterOffsetName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.OrbitalSpeedName), $"{nameof(OrbitalSpeedName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.OrbitalMaxRadiusName), $"{nameof(OrbitalMaxRadiusName)} is null");
                 Debug.Assert(!string.IsNullOrEmpty(data.OrbitalMinRadiusName), $"{nameof(OrbitalMinRadiusName)} is null");
+                Debug.Assert(!string.IsNullOrEmpty(data.OrbitalPosGainName), $"{nameof(OrbitalPosGainName)} is null");
+                Debug.Assert(!string.IsNullOrEmpty(data.OrbitalVelGainName), $"{nameof(OrbitalVelGainName)} is null");
+
                 Debug.Assert(!string.IsNullOrEmpty(data.StopDistanceName), $"{nameof(StopDistanceName)} is null");
+                Debug.Assert(!string.IsNullOrEmpty(data.PhaseFinalFollowGainName), $"{nameof(PhaseFinalFollowGainName)} is null");
             }
 
             [SerializeField, Tooltip("シェーダー")]
@@ -194,17 +225,19 @@ namespace Master.Modules
             [SerializeField, Tooltip("色バッファ名")]
             private string _colorBufferName;
 
-            [Header("グロバール")]
+            [Header("グローバル")]
             [SerializeField, Tooltip("パーティクル数パラメータ名")]
             private string _particleCountName;
-            [SerializeField, Tooltip("速度パラメータ名")]
-            private string _speedName;
             [SerializeField, Tooltip("中心位置パラメータ名")]
             private string _centerPositionName;
             [SerializeField, Tooltip("タイム名")]
             private string _timeName;
             [SerializeField, Tooltip("デルタタイム名")]
             private string _deltaTimeName;
+
+            [Header("フェーズ1")]
+            [SerializeField, Tooltip("放射拡散の加速度")]
+            private string _phase1AccelName;
 
             [Header("フェーズ2")]
             [SerializeField, Tooltip("円環最大半径パラメータ名")]
@@ -213,8 +246,13 @@ namespace Master.Modules
             private string _torusMinRadiusName;
             [SerializeField, Tooltip("円環の回転速度パラメータ名")]
             private string _torusSpeedName;
-            [SerializeField ,Tooltip("円環のYバネ力パラメータ名")]
+            [SerializeField, Tooltip("円環のYバネ力パラメータ名")]
             private string _torusYSpringName;
+            [SerializeField, Tooltip("接線方向の追従ゲイン")]
+            private string _torusTangentGainName;
+            [SerializeField, Tooltip("半径方向の拘束ゲイン")]
+            private string _torusRadiusGainName;
+
 
             [Header("フェーズ3")]
             [SerializeField, Tooltip("衛星球の数パラメータ名")]
@@ -229,10 +267,16 @@ namespace Master.Modules
             private string _orbitalMaxRadiusName;
             [SerializeField, Tooltip("衛星球の公転最小半径パラメータ名")]
             private string _orbitalMinRadiusName;
+            [SerializeField, Tooltip("追従PD制御の位置ゲイン")]
+            private string _orbitalPosGainName;
+            [SerializeField, Tooltip("追従PD制御の速度ゲイン")]
+            private string _orbitalVelGainName;
 
             [Header("フェーズファイナル")]
             [SerializeField, Tooltip("停止距離パラメータ名")]
             private string _stopDistanceName;
+            [SerializeField, Tooltip("文字への追従ゲイン")]
+            private string _phaseFinalFollowGainName;
         }
     }
 }
